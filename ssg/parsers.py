f4ceca7d386214a8ea91a72e53725cbd13b61772
Rahul Pandya <rapandya@microsoft.com>
2020-11-10 17:45:09 +0530

    second

---
+++ ssg/parsers.py
@@ -0,0 +1,30 @@
+from typing import List
+from pathlib import Path
+import shutil
+
+class Parser():
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return self.extensions.__contains__(extension)
+    
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise(NotADirectoryError)
+    
+    def read(self, path):
+        with open(path, "r") as file:
+            file.read()
+    
+    def write(self, path: Path, dest: Path, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
+    
+    def copy(path: Path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path, source, dest)
