847ae2c00d5660d54c081fb385c723349cbaa8d3
Rahul Pandya <rapandya@microsoft.com>
2020-11-10 18:24:27 +0530

    third fix

--- ssg/content.py
+++ ssg/content.py
@@ -1,21 +1,21 @@
 import re
 from typing import Dict
-from yaml import Loader, FullLoader
+from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
     @classmethod
-    def load(cls, Loader):
-        _, fm, content = Content.__regex.split(Loader, 2)
-        Content.load(fm, Loader = "FullLoader")
-        instance = cls()
+    def load(cls, string):
+        _, fm, content = Content.__regex.split(string, 2)
+        metadata = Content.load(fm, Loader = "FullLoader")
+        return cls(metadata, content)
     
     def __init__(self, metadata: Dict, content):
         self.data = metadata
-        self.data.update({"content": content})
+        self.data["content"] = content 
     
     @property
     def body(self):
@@ -35,11 +35,11 @@ class Content(Mapping):
     def __iter__(self):
         return self.data.__iter__()
     
-    def __len__(self) -> int: self.data.__len__()
+    def __len__(self) -> int: return len(self.data)
 
     def __repr__(self) -> str:
         data = {}
-        for (key, value) in self.data.items():
+        for key, value in self.data.items():
             if key != "content":
                 data[key] = value 
         return str(data)
